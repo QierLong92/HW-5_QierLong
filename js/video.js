a402ee0f22a545b6c1a45683539049a06a83f10f
Qier Long <longqi@umich.edu>
2025-11-23 20:20:40 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -5,11 +5,53 @@ window.addEventListener("load", function () {
 	video = this.document.querySelector("#player1");
 
 	video.controls = true;
+	/*Play and Pause*/
 	this.document.getElementById("play").addEventListener("click", function () {
 		video.play();
 		console.log("Play Video");
 	});
+	this.document.getElementById("pause").addEventListener("click", function () {
+		video.pause();
+		console.log("Pause Video");
+	});
+	/*slower and faster*/
+	this.document.getElementById("slower").addEventListener("click", function () {
+		video.playbackRate *= (1-0.1);
+		console.log("Current speed: ", video.playbackRate.toFixed(5));
+	});
+	this.document.getElementById("faster").addEventListener("click", function () {
+		video.playbackRate *= (1+0.1);
+		console.log("Current speed: ", video.playbackRate.toFixed(5));
+	});
+	/*skip ahead*/
+	this.document.getElementById("skip").addEventListener("click", function () {
+		if (video.currentTime + 10 >= video.duration) {
+			video.currentTime = 0;
+		} else {
+			video.currentTime += 10;
+		}
+		console.log("Current location: ", video.currentTime.toFixed(2));
+	});
+	/*mute and unmute*/
+	this.document.getElementById("mute").addEventListener("click", function() {
+			video.muted = !video.muted;
+			this.textContent = video.muted ? "Unmute" : "Mute";
+	});
+
+	/*volume slider*/
+	this.document.getElementById("slider").addEventListener("input", function() {
+        video.volume = this.value / 100;
+        document.getElementById("volume").innerText = this.value + "%";
+	});
+
+	/*two style changes*/
+	this.document.getElementById("vintage").addEventListener("click", function(){
+		video.classList.add("oldSchool");
+		console.log("Applied Old School style");
+	});
+	this.document.getElementById("orig").addEventListener("click", function(){
+		video.classList.remove("oldSchool");
+		console.log("Applied Original style");
+	});
 
 });
-
-
